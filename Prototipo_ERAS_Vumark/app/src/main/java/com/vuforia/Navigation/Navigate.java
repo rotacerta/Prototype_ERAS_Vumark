0bb138e68ee3fe1021060fef0d466567fc361cee
Davi Rocha Macedo Sousa <davirmsousa@hotmail.com>
2019-03-04 16:56:24 -0300

    Controle de Navegação
    Adicionado o controle de empilhamento de views (não permite) e leitura do botão físico BACK

--- Prototipo_ERAS_Vumark/app/src/main/java/com/vuforia/Navigation/Navigate.java
+++ Prototipo_ERAS_Vumark/app/src/main/java/com/vuforia/Navigation/Navigate.java
@@ -3,7 +3,10 @@ package com.vuforia.Navigation;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
+
+import com.vuforia.VuMark.VuMark;
 
 public class Navigate extends AppCompatActivity {
 
@@ -11,6 +14,7 @@ public class Navigate extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 goToActivity(v, classSelected);
+                finish();
             }
         });
     }
@@ -18,5 +22,17 @@ public class Navigate extends AppCompatActivity {
     public void goToActivity(View view, Class classSelected) {
         Intent intent = new Intent( view.getContext(), classSelected );
         startActivityForResult( intent, 0);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(!VuMark.class.getName().equals(event.getClass().getName())){
+            if(keyCode == KeyEvent.KEYCODE_BACK) {
+                startActivity(new Intent(this, VuMark.class));
+                finish();
+                return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
